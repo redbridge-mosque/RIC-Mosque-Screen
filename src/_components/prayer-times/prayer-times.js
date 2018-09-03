7e124f4d74f5905247d58f7876473bc536cc0219
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2018-09-03 19:49:55 +0100

    now hightlights next prayer time

--- src/_components/prayer-times/prayer-times.js
+++ src/_components/prayer-times/prayer-times.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import moment from 'moment';
 import './prayer-times.css';
 import data from '../../_assets/data/elm-prayer-times-2018.json';
+import NextJamahTime from '../next-jamah-time/next-jamah-time';
 
 class PrayerTimes extends Component {
   constructor(props) {
     super(props);
     this.state = {
       prayerTimes: this.getPrayerTimes(),
-      tomorrowsPrayerTimes: this.getTomorrowsPrayerTimes()
+      tomorrowsPrayerTimes: this.getTomorrowsPrayerTimes(),
+      nextJamah: this.getNextJammah()
     };
   }
 
@@ -22,6 +24,11 @@ class PrayerTimes extends Component {
       .add(1, 'days')
       .format('DD/MM/YYYY');
     return data[date];
+  }
+
+  getNextJammah() {
+    var nextJamah = new NextJamahTime();
+    return nextJamah.getNextJamahTime();
   }
 
   render() {
@@ -40,31 +47,71 @@ class PrayerTimes extends Component {
             <tr>
               <th>Fajr</th>
               <td>{this.state.prayerTimes['Fajr Begins']}</td>
-              <td>{this.state.prayerTimes['Fajr Jama‘ah']}</td>
+              <td
+                className={
+                  this.state.nextJamah.name === 'Fajr'
+                    ? 'nextJamahHighlight'
+                    : null
+                }
+              >
+                {this.state.prayerTimes['Fajr Jama‘ah']}
+              </td>
               <td>{this.state.tomorrowsPrayerTimes['Fajr Jama‘ah']}</td>
             </tr>
             <tr>
               <th>Zuhr</th>
               <td>{this.state.prayerTimes['Zuhr Begins']}</td>
-              <td>{this.state.prayerTimes['Zuhr Jama‘ah']}</td>
+              <td
+                className={
+                  this.state.nextJamah.name === 'Zuhr'
+                    ? 'nextJamahHighlight'
+                    : null
+                }
+              >
+                {this.state.prayerTimes['Zuhr Jama‘ah']}
+              </td>
               <td>{this.state.tomorrowsPrayerTimes['Zuhr Jama‘ah']}</td>
             </tr>
             <tr>
               <th>'Asr</th>
               <td>{this.state.prayerTimes['Asr Mithl 2']}</td>
-              <td>{this.state.prayerTimes['Asr Jama‘ah']}</td>
+              <td
+                className={
+                  this.state.nextJamah.name === 'Asr'
+                    ? 'nextJamahHighlight'
+                    : null
+                }
+              >
+                {this.state.prayerTimes['Asr Jama‘ah']}
+              </td>
               <td>{this.state.tomorrowsPrayerTimes['Asr Jama‘ah']}</td>
             </tr>
             <tr>
               <th>Maghrib</th>
               <td>{this.state.prayerTimes['Maghrib Begins']}</td>
-              <td>{this.state.prayerTimes['Maghrib Jama‘ah']}</td>
+              <td
+                className={
+                  this.state.nextJamah.name === 'Maghrib'
+                    ? 'nextJamahHighlight'
+                    : null
+                }
+              >
+                {this.state.prayerTimes['Maghrib Jama‘ah']}
+              </td>
               <td>{this.state.tomorrowsPrayerTimes['Maghrib Jama‘ah']}</td>
             </tr>
             <tr>
               <th>Isha</th>
               <td>{this.state.prayerTimes['Isha Begins']}</td>
-              <td>{this.state.prayerTimes['Isha Jama‘ah']}</td>
+              <td
+                className={
+                  this.state.nextJamah.name === 'Isha'
+                    ? 'nextJamahHighlight'
+                    : null
+                }
+              >
+                {this.state.prayerTimes['Isha Jama‘ah']}
+              </td>
               <td>{this.state.tomorrowsPrayerTimes['Isha Jama‘ah']}</td>
             </tr>
           </tbody>
